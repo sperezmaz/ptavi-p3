8a5b554cf96447a6fdabd06816c205f12b18c26b
sperezmaz <s.perezmaz@alumnos.urjc.es>
2017-10-15 20:13:19 +0200

    ejercio3 term

--- smallsmilhandler.py
+++ smallsmilhandler.py
@@ -7,69 +7,40 @@ from xml.sax.handler import ContentHandler
 class SmallSMILHandler(ContentHandler):
 
     def __init__ (self):
-            """
-            Constructor. Inicializamos las variables
-            """
-            self.width = ""
-            self.height = ""
-            self.background-color = ""
-            self.id = ""
-            self.top = ""
-            self.bottom = ""
-            self.left = ""
-            self.right = ""
-            self.src = ""
-            self.region = ""
-            self.begin = ""
-            self.dur = ""
-            self.end = ""
-            self.variable = []
+        """
+        Constructor. Inicializamos las variables
+        """
+        self.variable = []
             
     def startElement(self, name, attrs):
         """
         Método que se llama cuando se abre una etiqueta
         """
-        if name == 'root_layout':
-            # De esta manera tomamos los valores de los atributos
-            self.width = attrs.get('width', "")
-            self.height = attrs.get('height', "")
-            self.background_color = attrs.get('background_color', "")
-            self.variable.append(self.width)
-            self.variable.append(self.height)
-            self.variable.append(self.background_color)
+        tags = {'root-layout': {'width', 'height', 'background-color'},
+                'region': {'id', 'top', 'left', 'boottom', 'right'},
+                'img': {'src', 'region', 'begin', 'dur'},
+                'audio': {'src', 'begin', 'dur'},
+                'textstream': {'src', 'region'}}
+        atribcont = {}
+        
+        if name in tags:
             
-        elif name == 'region':
-            self.id = attrs.get('id', "")
-            self.top = attrs.get('top', "")
-            self.bottom = attrs.get('bottom', "")
-            self.left = attrs.get('left', "")
-            self.right = attrs.get('right', "")
-            self.variable.append(self.id)
-            self.variable.append(self.top)
-            self.variable.append(self.bottom)
-            self.variable.append(self.left)
-            self.variable.append(self.right)
+            for atribute in tags[name]:
+                atribcont[atribute] = attrs.get(atribute, "")
+                tagsentero = {name: atribcont}
             
-        elif name == 'img':
-            self.src = attrs.get('src', "")
-            self.region = attrs.get('region', "")
-            self.begin = attrs.get('begin', "")
-            self.dur = attrs.get('dur', "")
-            self.variable.append(self.src)
-            self.variable.append(self.region)
-            self.variable.append(self.begin)
-            self.variable.append(self.dur)
-           
-        elif name == 'audio':
-            self.src = attrs.get('src', "")
-            self.begin = attrs.get('begin', "")
-            self.dur = attrs.get('dur', "")
-            self.variable.append(self.src)
-            self.variable.append(self.begin)
-            self.variable.append(self.dur)       
+            self.variable.append(tagsentero)
+                
+    def get_tags(self):
+        return self.variable
             
-        elif name == 'textstream':    
-            self.src = attrs.get('src', "")
-            self.region = attrs.get('region', "")
-            self.variable.append(self.src)
-            self.variable.append(self.region)
+if __name__ == "__main__":
+    """
+    Programa principal
+    """
+    parser = make_parser()
+    cHandler = SmallSMILHandler()
+    parser.setContentHandler(cHandler)
+    parser.parse(open('karaoke.smil'))
+    print(cHandler.get_tags())
+
