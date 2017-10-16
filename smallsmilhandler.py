ec536148d2aae45f07682b16803c0fb18f0dd484
sperezmaz <s.perezmaz@alumnos.urjc.es>
2017-10-16 14:23:37 +0200

    ejercio3 pep8

--- smallsmilhandler.py
+++ smallsmilhandler.py
@@ -4,36 +4,37 @@
 from xml.sax import make_parser
 from xml.sax.handler import ContentHandler
 
+
 class SmallSMILHandler(ContentHandler):
 
-    def __init__ (self):
+    def __init__(self):
         """
         Constructor. Inicializamos las variables
         """
         self.variable = []
-            
+
     def startElement(self, name, attrs):
         """
         Método que se llama cuando se abre una etiqueta
         """
         tags = {'root-layout': {'width', 'height', 'background-color'},
-                'region': {'id', 'top', 'left', 'boottom', 'right'},
+                'region': {'id', 'top', 'bottom', 'left', 'right'},
                 'img': {'src', 'region', 'begin', 'dur'},
                 'audio': {'src', 'begin', 'dur'},
                 'textstream': {'src', 'region'}}
         atribcont = {}
-        
+
         if name in tags:
-            
+
             for atribute in tags[name]:
                 atribcont[atribute] = attrs.get(atribute, "")
                 tagsentero = {name: atribcont}
-            
+
             self.variable.append(tagsentero)
-                
+
     def get_tags(self):
         return self.variable
-            
+
 if __name__ == "__main__":
     """
     Programa principal
@@ -43,4 +44,3 @@ if __name__ == "__main__":
     parser.setContentHandler(cHandler)
     parser.parse(open('karaoke.smil'))
     print(cHandler.get_tags())
-
